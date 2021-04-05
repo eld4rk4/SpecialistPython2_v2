83d7260227d45bce6111473953699d60056734b3
eld4rk4 <81983162+eld4rk4@users.noreply.github.com>
2021-04-05 20:08:09 +0300

    Update 01_distance.py

--- Module1/practice/points/01_distance.py
+++ Module1/practice/points/01_distance.py
@@ -15,7 +15,10 @@ points1 = Point(2, 4)
 point2 = Point(5, -2)
 
 # Задание: Найдите расстояние между этими точками. Реализовав и используя функцию distance()
-
+a=point2.x-points1.x
+b=point2.y-points1.y
+from math import sqrt
+c=sqrt(a*a+b*b)
 # TODO: your core here...
 
-print("Расстояние между точками = ", ...)
+print("Расстояние между точками = ", a,b,c)
